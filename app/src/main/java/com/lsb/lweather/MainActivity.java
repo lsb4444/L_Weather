7f66063899b2a3d985e68efcb1f24316a0cb8b0f
L <allfogg@naver.com>
2017-11-01 13:56:13 +0900

    Firebase 연결, 처음 화면 구름 이미지 추가

--- app/src/main/java/com/lsb/lweather/MainActivity.java
+++ app/src/main/java/com/lsb/lweather/MainActivity.java
@@ -4,18 +4,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 import io.realm.Realm;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private Fragment mSerc;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
